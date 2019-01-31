d6dd07376f8078f6e541458e45089bacbaa25ab9
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-01-31 12:14:48 +0100

    Remove gatsby-browser, update gatsby-config, introduce styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chloe Kuypers | Web Developer`,
+    description: `My portfolio, built with Gatsby`,
+    author: `@letitia-may`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,67 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
+import styled from 'styled-components'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const MainLayout = styled.div`
+  height: 100vh;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 1rem 1.5rem;
+  background-color: #f7f8f8;
+`
+
+const MainContent = styled.div`
+  text-align: center;
+`
+
+const BigTitle = styled.h1`
+  color: #333a44;
+  text-transform: uppercase;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 300;
+`
+
+const LittleTitle = styled.h2`
+  color: #333a44;
+  text-transform: uppercase;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 300;
+`
+
+const Line = styled.hr`
+  max-width: 800px;
+  width: 100%;
+  border-top: 1px solid #8de3db;
+
+  &:after {
+    content: ' ';
+    height: 8px;
+    position: relative;
+    bottom: 4px;
+    left: calc(50% - 4px);
+    width: 8px;
+    background: #8de3db;
+    transform: rotate(45deg);
+    display: block;
+  }
+`
+
 const IndexPage = () => (
-  <Layout>
+  <MainLayout>
     <SEO title="Home" keywords={[`Chloe Kuypers`, `development`]} />
-    <h1>Chloe Kuypers</h1>
-    <p>Web developer</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <MainContent>
+      <BigTitle>Chloe Kuypers</BigTitle>
+      <Line />
+      <LittleTitle>Web developer</LittleTitle>
+    </MainContent>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+  </MainLayout>
 )
 
 export default IndexPage
