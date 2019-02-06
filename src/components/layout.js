3e3b5da964769d0566431b9415fe95d9c8d8cbd1
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-02-06 11:22:40 +0100

    Update footer style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Header from './header'
 import GlobalStyle from '../styles/global'
 import '../styles/normalize.css'
+
+const PageContent = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
+
+const Footer = styled.footer`
+  border-top: 1px solid var(--pink);
+  margin: 1rem;
+  padding: 1rem;
+  text-align: right;
+  font-size: 0.75rem;
+  line-height: 1.5;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -21,21 +38,18 @@ const Layout = ({ children }) => (
       <>
         <GlobalStyle />
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <PageContent>{children}</PageContent>
+        <Footer>
+          © {new Date().getFullYear()}, Built with{` `}
+          <a href="https://www.gatsbyjs.org" target="_blank">
+            Gatsby
+          </a>
+          <br />
+          Source available on{` `}
+          <a href="https://github.com/Letitia-May/portfolio" target="_blank">
+            Github
+          </a>
+        </Footer>
       </>
     )}
   />
