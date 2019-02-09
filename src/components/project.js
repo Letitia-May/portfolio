c3f01407d40fead05835e2ad1979cd2a939a201c
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-02-09 09:26:36 +0100

    Change project component from functional to class, on click show info about project

--- src/components/project.js
+++ src/components/project.js
@@ -4,8 +4,6 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   width: 700px;
-  background-color: var(--teal);
-  padding: 2rem 1rem;
   text-align: center;
   margin: 2rem 0;
   color: var(--white);
@@ -15,7 +13,17 @@ const Container = styled.div`
   }
 `
 
-const Title = styled.p`
+const StyledMainTile = styled.div`
+  padding: 2rem 1rem;
+  background-color: var(--teal);
+`
+
+const StyledInfoTile = styled.div`
+  padding: 2rem 1rem;
+  background-color: var(--light-grey);
+`
+
+const Heading = styled.p`
   font-size: 2.5rem;
   font-weight: 800;
 
@@ -24,11 +32,19 @@ const Title = styled.p`
   }
 `
 
-// const Info = styled.a`
-//   color: var(--white);
-//   text-decoration: underline;
-//   font-size: 0.75rem;
-// `;
+const InfoLink = styled.a`
+  color: var(--white);
+  font-size: 0.75rem;
+  border-bottom: 1px solid var(--white);
+  transition: all 0.35s ease-in-out;
+
+  &:hover {
+    color: var(--dark-grey);
+    cursor: pointer;
+    font-weight: 600;
+    border-bottom: 1px solid var(--dark-grey);
+  }
+`
 
 const Button = styled.button`
   border: 1px solid var(--white);
@@ -40,22 +56,71 @@ const Button = styled.button`
   }
 `
 
-const Project = ({ url, title }) => (
-	<Container>
-		{/* <Info>more info</Info> */}
-		<Title>{ title }</Title>
-		<a
-			href={ url }
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			<Button>Visit</Button>
-		</a>
-	</Container>
-)
+const BackButton = styled(Button)`
+  background-color: var(--light-grey);
+
+  &:hover {
+    background-color: var(--teal);
+  }
+`
+
+const ProjectInfo = styled.p`
+  @media (max-width: 800px) {
+    font-size: 0.75rem;
+  }
+`
+
+class Project extends React.Component {
+  constructor(props) {
+    super(props)
+    this.showInfoTile = this.showInfoTile.bind(this)
+    this.showMainTile = this.showMainTile.bind(this)
+    this.state = { isMoreInfoClicked: false }
+  }
+
+  showInfoTile() {
+    this.setState({ isMoreInfoClicked: true })
+  }
+
+  showMainTile() {
+    this.setState({ isMoreInfoClicked: false })
+  }
+
+  render() {
+    const isMoreInfoClicked = this.state.isMoreInfoClicked
+    let tile
+
+    const MainTile = () => (
+      <StyledMainTile>
+        <InfoLink onClick={this.showInfoTile}>more info</InfoLink>
+        <Heading>{this.props.title}</Heading>
+        <a href={this.props.url} target="_blank" rel="noopener noreferrer">
+          <Button>Visit</Button>
+        </a>
+      </StyledMainTile>
+    )
+
+    const InfoTile = () => (
+      <StyledInfoTile>
+        <ProjectInfo>{this.props.about}</ProjectInfo>
+        <BackButton onClick={this.showMainTile}>Back</BackButton>
+      </StyledInfoTile>
+    )
+
+    if (isMoreInfoClicked) {
+      tile = <InfoTile />
+    } else {
+      tile = <MainTile />
+    }
+
+    return <Container>{tile}</Container>
+  }
+}
 
 Project.propTypes = {
   url: PropTypes.string,
+  title: PropTypes.string,
+  about: PropTypes.string,
 }
 
 export default Project
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,14 +4,21 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Project from '../components/project'
 
-
 const Projects = () => (
   <>
     <Layout>
       <SEO title="Projects" />
 
-      <Project url="https://capricornrecords.com.au/" title="Capricorn Records" />
-      <Project url="https://techwhen.com/" title="TechWhen" />
+      <Project
+        url="https://capricornrecords.com.au/"
+        title="Capricorn Records"
+        about="Built using Shopify CMS with custom media queries... add some more here"
+      />
+      <Project
+        url="https://techwhen.com/"
+        title="TechWhen"
+        about="Work in progress - working on this project with another dev to practive React skills, and learn more about Docker, AWS, Material Design"
+      />
     </Layout>
   </>
 )
