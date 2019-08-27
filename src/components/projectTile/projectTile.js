a654afefc4d589b4fc7e77599d3798c3a2b88604
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-08-27 22:38:01 +0200

    Show project tile on hover instead of on click

--- src/components/projectTile/projectTile.js
+++ src/components/projectTile/projectTile.js
@@ -1,44 +1,35 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+
 import styles from './projectTile.module.scss'
 
 const ProjectTile = ({ url, title, about }) => {
   const [showMoreInfo, setShowMoreInfo] = useState(false)
 
-  let showInfoTile = () => {
-    setShowMoreInfo(true)
-  }
-
-  let showMainTile = () => {
-    setShowMoreInfo(false)
-  }
-
   const tileClass = classNames({
     [styles.tile_main]: !showMoreInfo,
-    [styles.tile_info]: showMoreInfo
-  });
+    [styles.tile_info]: showMoreInfo,
+  })
 
   return (
-    <div className={classNames(styles.tile, tileClass)}>
+    <div
+      className={classNames(styles.tile, tileClass)}
+      onMouseEnter={() => setShowMoreInfo(true)}
+      onMouseLeave={() => setShowMoreInfo(false)}
+    >
       {!showMoreInfo ? (
         <>
-          <a className={styles.info_link} onClick={showInfoTile}>
-            more info
-          </a>
           <p className={styles.heading}>{title}</p>
+        </>
+      ) : (
+        <>
+          <p className={styles.project_info}>{about}</p>
           <a href={url} target="_blank" rel="noopener noreferrer">
             <button className={styles.button}>Visit</button>
           </a>
         </>
-      ) : (
-          <>
-            <p className={styles.project_info}>{about}</p>
-            <button className={styles.back_button} onClick={showMainTile}>
-              Back
-          </button>
-          </>
-        )}
+      )}
     </div>
   )
 }
