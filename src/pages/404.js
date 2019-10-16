2191ee25d88a038fed9aa76ba8d804418bb963dc
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-10-16 13:43:16 +0200

    Update content on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout/layout'
 import SEO from '../components/seo/seo'
@@ -7,7 +8,12 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist.</p>
+    <p>Try visiting the <Link to="/"
+      style={{ color: `var(--primary)` }}>home page
+      </Link> or checking out my <Link to="/projects/"
+        style={{ color: `var(--primary)` }}>projects</Link>.
+    </p>
   </Layout>
 )
 
